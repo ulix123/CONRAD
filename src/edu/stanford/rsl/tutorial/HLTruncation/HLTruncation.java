908256d4ab75ccb5bf6b52affb81bc3fdd18eadc
ulix123 <Kim_Vo@web.de>
2016-01-30 19:19:17 +0100

    Implementation of HL-based truncation correction

--- src/edu/stanford/rsl/tutorial/HLTruncation/HLTruncation.java
+++ src/edu/stanford/rsl/tutorial/HLTruncation/HLTruncation.java
@@ -1,142 +1,150 @@
 package edu.stanford.rsl.tutorial.HLTruncation;
 
+import ij.IJ;
+
+import java.util.Arrays;
+
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
 
 public class HLTruncation {
-	private float truncThreshold;
+	private float truncationThreshold;
 	private int sinogramExtensionSize;
 	
+	
 	public HLTruncation(float truncThreshold) {
-		this.truncThreshold = truncThreshold;
-		
+		this.truncationThreshold = truncThreshold;
 	}
 	
-	public Grid2D CorrectTruncation(Grid2D sinogram) {
+	public Grid2D correctTruncation(Grid2D sinogram) {
+		IJ.showStatus("Computing mean mass of non-truncated projections...");
 		float[] massPerProjection = getMassPerProjection(sinogram);
+		IJ.showStatus("Retrieving truncated projections...");
 		boolean[] truncatedProjections = getTruncatedProjections(massPerProjection);
-		float meanMassNonTruncProjections = getMeanMassNonTruncProjections(massPerProjection,truncatedProjections);
-		float[][] WidthTruncationCorrection = getWidthTruncationCorrection(sinogram,massPerProjection,truncatedProjections,meanMassNonTruncProjections);
-		Grid2D extendedSinogram = extendedSinogram(sinogram);
-		Grid2D fixSino = extrapolateTruncation(extendedSinogram,truncatedProjections,WidthTruncationCorrection);
-		return fixSino;
-	}
-	public Grid2D extendedSino(Grid2D sinogram){
-		float[] massPerProjection = getMassPerProjection(sinogram);
-		boolean[] truncatedProjections = getTruncatedProjections(massPerProjection);
-		float meanMassNonTruncProjections = getMeanMassNonTruncProjections(massPerProjection,truncatedProjections);
-		//float[][] WidthTruncationCorrection = getWidthTruncationCorrection(sinogram,massPerProjection,truncatedProjections,meanMassNonTruncProjections);
-		Grid2D extendedSinogram = extendedSinogram(sinogram);
+		float meanMass = getMeanMassNonTruncatedProjections(massPerProjection, truncatedProjections);
+		System.out.println("Mean mass of non-truncated projections: "+Float.toString(meanMass));
+		float[][] widthTruncationCorrection = getWidthTruncationCorrection(sinogram, meanMass, massPerProjection, truncatedProjections);
+		/*for (int i=0; i<massPerProjection.length; i++) {
+			System.out.println(Float.toString(widthTruncationCorrection[i][0])+"       "+Float.toString(widthTruncationCorrection[i][1]));
+			
+		}
+		*/
+		Grid2D extendedSinogram = extendSinogram(sinogram);
+		extendedSinogram = extrapolateTruncation(extendedSinogram, widthTruncationCorrection, truncatedProjections);
+		
+		
 		return extendedSinogram;
 	}
-
-	public float[] getMassPerProjection( Grid2D sinogram){
+	
+	public float[] getMassPerProjection(Grid2D sinogram) {
 		int nProjections = sinogram.getHeight();
 		int nDetectorPixels = sinogram.getWidth();
 		float[] massPerProjection = new float[nProjections];
-		
-		for(int cproj=0; cproj < nProjections; cproj++){
+		for (int cproj=0; cproj<nProjections; cproj++) {
 			massPerProjection[cproj] = 0;
-			for(int detectorPixels = 0; detectorPixels < nDetectorPixels; detectorPixels++){
-				massPerProjection[cproj] += sinogram.getPixelValue(detectorPixels,cproj);
+			for (int detectorPixel=0; detectorPixel<nDetectorPixels; detectorPixel++) {
+				massPerProjection[cproj] += sinogram.getPixelValue(detectorPixel, cproj);
 			}
 		}
-		return massPerProjection;		
+		return massPerProjection;
 	}
 	
-	private boolean[] getTruncatedProjections(float[] massPerProjection){
-		
+	private boolean[] getTruncatedProjections(float[] massPerProjection) {
 		boolean[] truncatedProjections = new boolean[massPerProjection.length];
-		
-		for(int cproj = 0; cproj < massPerProjection.length; cproj++){
-			if(massPerProjection[cproj] < truncThreshold){
+		for (int cproj=0; cproj<massPerProjection.length; cproj++) {
+			if (massPerProjection[cproj] < truncationThreshold) {
 				truncatedProjections[cproj] = true;
-			}else{
+			} else {
 				truncatedProjections[cproj] = false;
-			}	
+			}
 		}
-		return truncatedProjections;	
+		return truncatedProjections;
 	}
-	 
-	private float getMeanMassNonTruncProjections(float[] massPerProjections,boolean[] truncatedProjections){
-		float sumNonTruncMass = 0.f;
+	
+	private float getMeanMassNonTruncatedProjections(float[] massPerProjection, boolean[] truncatedProjections) {
+		float meanMass = 0f;
 		int countNonTruncatedProjections = 0;
-		
-		for(int cproj = 0; cproj < massPerProjections.length; cproj++){
-			if (!truncatedProjections[cproj]){
-				sumNonTruncMass += massPerProjections[cproj];
+		for (int cproj=0; cproj<massPerProjection.length; cproj++) {
+			if (!truncatedProjections[cproj]) {
 				countNonTruncatedProjections++;
+				meanMass += massPerProjection[cproj];
 			}
 		}
-		return sumNonTruncMass/countNonTruncatedProjections;
+		meanMass = meanMass / countNonTruncatedProjections;
+		return meanMass;
 	}
 	
-	private float[][] getWidthTruncationCorrection(Grid2D sinogram, float[] massPerProjections,boolean[] truncatedProjections,float meanMassNonTruncProjections){
-		
-		float[][] WidthTruncCorr = new float[massPerProjections.length][2];
-		float maxWidth = 0.f;
-				
-		for(int cproj = 0; cproj < massPerProjections.length; cproj++){
-			if(truncatedProjections[cproj]){
-				float MassDiff = meanMassNonTruncProjections - massPerProjections[cproj];
-				float greyValueLeft = sinogram.getPixelValue(0,cproj);
+	private float[][] getWidthTruncationCorrection(Grid2D sinogram, float meanMass, float[] massPerProjection, boolean[] truncatedProjections) {
+		float[][] widthTruncCorr = new float[massPerProjection.length][2];
+		float maxWidth = 0f;
+		for (int cproj=0; cproj<massPerProjection.length; cproj++){
+			if (truncatedProjections[cproj]) {
+				float massDifference = meanMass - massPerProjection[cproj];
+				float greyValueLeft = sinogram.getPixelValue(0, cproj);
 				float greyValueRight = sinogram.getPixelValue(sinogram.getWidth()-1, cproj);
-				float massLeft = MassDiff*(1-1/(1+greyValueLeft/greyValueRight));
-				float massRight = MassDiff - massLeft;
-				//links 0 rechts 1
-				WidthTruncCorr[cproj][0] = 2*massLeft/greyValueLeft;
-				WidthTruncCorr[cproj][1] = 2*massRight/greyValueRight;
-				if(WidthTruncCorr[cproj][0] > maxWidth){
-					maxWidth = WidthTruncCorr[cproj][0];
+				float massLeft = massDifference * (1 - 1/(1+greyValueLeft/greyValueRight));
+				float massRight = massDifference - massLeft;
+				widthTruncCorr[cproj][0] = 2 * massLeft/greyValueLeft;
+				widthTruncCorr[cproj][1] = 2 * massRight/greyValueRight;
+				if (widthTruncCorr[cproj][0] > maxWidth) {
+					maxWidth = widthTruncCorr[cproj][0];
 				}
-				if(WidthTruncCorr[cproj][1] > maxWidth){
-					maxWidth = WidthTruncCorr[cproj][1];
+				if (widthTruncCorr[cproj][1] > maxWidth) {
+					maxWidth = widthTruncCorr[cproj][1];
 				}
-			}else{
-				WidthTruncCorr[cproj][0] = 0;
-				WidthTruncCorr[cproj][1] = 0;
+				
+			} else {
+				widthTruncCorr[cproj][0] = 0;
+				widthTruncCorr[cproj][1] = 0;
 			}
 		}
-		sinogramExtensionSize = (int)maxWidth +1;
-		return WidthTruncCorr;	
+		sinogramExtensionSize = (int) maxWidth + 1;
+		return widthTruncCorr;
 	}
 	
-	private Grid2D extendedSinogram(Grid2D sinogram){
-		Grid2D extendedSinogram = new Grid2D(sinogram.getWidth()+2*sinogramExtensionSize,sinogram.getHeight());
+	private Grid2D extendSinogram(Grid2D sinogram) {
+		Grid2D extendedSinogram = new Grid2D(sinogram.getWidth() + 2*sinogramExtensionSize, sinogram.getHeight());
 		
-		for(int x = 0; x < extendedSinogram.getWidth(); x++){
-			for(int y = 0; y < extendedSinogram.getHeight(); y++){
-				if( x < sinogramExtensionSize || x >= extendedSinogram.getWidth() - sinogramExtensionSize){
-					extendedSinogram.putPixelValue(x, y, 0);		
-				}else{
-					extendedSinogram.putPixelValue(x, y, sinogram.getPixelValue(x-sinogramExtensionSize, y));
+		for (int x=0; x<extendedSinogram.getWidth(); x++) {
+			for (int y=0; y<extendedSinogram.getHeight(); y++) {
+				if (x < sinogramExtensionSize || x >= extendedSinogram.getWidth() - sinogramExtensionSize) {
+					extendedSinogram.putPixelValue(x, y, 0);
+				} else {
+					extendedSinogram.putPixelValue(x, y, sinogram.getPixelValue(x - sinogramExtensionSize, y));
 				}
 			}
 		}
 		return extendedSinogram;
 	}
 	
-	private Grid2D extrapolateTruncation(Grid2D extendedSinogram, boolean[] truncatedProjections, float[][] WidthTruncCorr){
-		
-		for(int cproj = 0; cproj < truncatedProjections.length;cproj++){
-			if(truncatedProjections[cproj]){
-				float greyValueLeft = extendedSinogram.getPixelValue(sinogramExtensionSize+1,cproj);
-				float greyValueRight = extendedSinogram.getPixelValue(extendedSinogram.getWidth()- sinogramExtensionSize-1,cproj);
-				float slopeLeft = greyValueLeft/WidthTruncCorr[cproj][0];
-				float slopeRight = greyValueRight/WidthTruncCorr[cproj][1];
-				//vom linken Bildrand bis eins vor dem linken Punkt
-				for(int i = 0; i < (int)WidthTruncCorr[cproj][0];i++){
-					int leftIdx = (int) (sinogramExtensionSize+1 - WidthTruncCorr[cproj][0])+i;
-					float cVal = i*slopeLeft;
+	private Grid2D extrapolateTruncation(Grid2D extendedSinogram, float[][] widthTruncationCorrection, boolean[] truncatedProjections) {
+		for (int cproj=0; cproj<truncatedProjections.length; cproj++) {
+			IJ.showProgress(cproj, truncatedProjections.length);
+			IJ.showStatus("Extrapolating truncated projections...");
+			if (truncatedProjections[cproj]) {
+				float greyValueLeft = extendedSinogram.getPixelValue(sinogramExtensionSize+1, cproj);
+				float greyValueRight = extendedSinogram.getPixelValue(extendedSinogram.getWidth() - sinogramExtensionSize - 1, cproj);
+				float slopeLeft = greyValueLeft / widthTruncationCorrection[cproj][0];
+				float slopeRight = greyValueRight / widthTruncationCorrection[cproj][1];
+				for (int i=0; i<(int)widthTruncationCorrection[cproj][0]; i++) {
+					int leftIdx = sinogramExtensionSize - (int)widthTruncationCorrection[cproj][0] + i;
+					float cVal = i * slopeLeft;
 					extendedSinogram.putPixelValue(leftIdx, cproj, cVal);
-				}	
-				//von rechtem i+1 punkt zum rechten Bildrand
-				for(int j = 0; j < (int)WidthTruncCorr[cproj][1];j++){	
-					int rightIdx = (int) (extendedSinogram.getWidth() - sinogramExtensionSize+j);
-					float cVal = greyValueRight - (j+1) * slopeRight;
+				}
+				for (int i=0; i<(int)widthTruncationCorrection[cproj][1]; i++) {
+					int rightIdx = extendedSinogram.getWidth() - sinogramExtensionSize + i;
+					float cVal = greyValueRight - (i+1) * slopeRight;
 					extendedSinogram.putPixelValue(rightIdx, cproj, cVal);
 				}
+				
 			}
+			
 		}
+		
 		return extendedSinogram;
 	}
+	
+	public void setTruncationThreshold(float threshold) {
+		this.truncationThreshold = threshold;
+	}
+
 }
--- src/edu/stanford/rsl/tutorial/HLTruncation/HLTruncationExample.java
+++ src/edu/stanford/rsl/tutorial/HLTruncation/HLTruncationExample.java
@@ -1,27 +1,34 @@
 package edu.stanford.rsl.tutorial.HLTruncation;
 
+import ij.IJ;
+import ij.ImageJ;
 import edu.stanford.rsl.conrad.data.numeric.Grid2D;
 import edu.stanford.rsl.tutorial.parallel.ParallelProjector2D;
 import edu.stanford.rsl.tutorial.phantoms.Ellipsoid;
 import edu.stanford.rsl.tutorial.phantoms.SheppLogan;
 
 public class HLTruncationExample {
-	public static void main(String [] args) {
-		//phantoms
-		Ellipsoid elli = new Ellipsoid(200,150);
-		SheppLogan phantom = new SheppLogan(200, false);
-		///
-		ParallelProjector2D proj = new ParallelProjector2D(Math.PI,Math.PI/360,110,0.4);
-		Grid2D sino = proj.projectRayDriven(elli);//oder elli
-		sino.show("sinogram");
-		
-		HLTruncation truncationFix = new HLTruncation(35000);
-		Grid2D corrSino = truncationFix.CorrectTruncation(sino);
-		Grid2D extendedSino = truncationFix.extendedSino(sino);
-		extendedSino.show("extended sino");
-		corrSino.show("corrected sinogram");
+	
+	public static void main(String[] arg) {
+		new ImageJ();
+		SheppLogan sebbi = new SheppLogan(200, false);
+		Ellipsoid elli = new Ellipsoid(200, 150);
+		ParallelProjector2D proj = new ParallelProjector2D(Math.PI, Math.PI/360.0, 165, 0.4); //165
+		Grid2D sinosebbi = proj.projectRayDriven(sebbi);
+		proj = new ParallelProjector2D(Math.PI, Math.PI/360.0, 120, 0.4);
+		Grid2D sinoelli = proj.projectRayDriven(elli);
+		sinosebbi.show("Sinogram Shepp-Logan");
+		sinoelli.show("Sinogram ellipsodes");
+		HLTruncation hl = new HLTruncation(12260); //SheppLogan parameter
+		Grid2D corrSinoSebbi = hl.correctTruncation(sinosebbi);
+		hl.setTruncationThreshold(35100);
+		Grid2D corrSinoElli = hl.correctTruncation(sinoelli);
+		corrSinoSebbi.show("Corrected sinogram Shepp-Logan");
+		corrSinoElli.show("Corrected sinogram ellipsoids");
 		
 	}
 	
 	
+	
+
 }
